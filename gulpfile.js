62caba807381766dd512a189ced70676e729537e
Shadi Isber <shadiisber@Shadis-MacBook-Pro.local>
2018-01-01 21:00:07 -0500

    testing casing modifying for css modules

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,29 @@
 var gulp = require('gulp');
 var postcss = require('gulp-postcss');
 
-var autoprefixer = require('autoprefixer');
+var pattern = /[-_.]+([a-zA-Z0-9])/g;
+
+function replacer(g) {
+  var character = g[g.length - 1];
+  return g[0] === '.' ? '.' + character.toLowerCase() : character.toUpperCase();
+}
 
 /**
  * Test plugin here
  */
 var testPlugin = require('postcss').plugin('postcss-test', function myplugin(options) {
-	const propertyMatch = /^overflow$/;
-
-    return function (css) {
-		css.walkDecls(propertyMatch, (decl) => {
-			decl.cloneBefore({
-				prop: 'white-space',
-				value: 'nowrap'
-			});
-			decl.cloneBefore({
-				prop: 'overflow',
-				value: 'hidden'
-			});
-			decl.cloneBefore({
-				prop: 'text-overflow',
-				value: 'ellipsis'
-			});
-
-			decl.remove();
-		});
- 
-    }
+  return function(css) {
+    css.walkRules(rule => {
+      rule.selector = rule.selector.replace(pattern, replacer);
+    });
+  };
 });
 /*** End test plugin ***/
 
-
-gulp.task('default', function () {
-  var processors = [
-    testPlugin,
-    autoprefixer
-  ];
-  return gulp.src('./src/*.css')
+gulp.task('default', function() {
+  var processors = [testPlugin];
+  return gulp
+    .src('./src/*.css')
     .pipe(postcss(processors))
     .pipe(gulp.dest('./dest'));
 });
